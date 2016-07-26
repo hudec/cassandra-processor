a7bebd8e3a3a371072f10247481ac4202696a37f
Vladimír Hudec <Vladimir.Hudec@gmail.com>
2016-07-26 09:26:18 +0200

    Issue #1: Initial code

--- cassandra-processor/src/test/java/org/sqlproc/engine/impl/TestBasic.java
+++ cassandra-processor/src/test/java/org/sqlproc/engine/impl/TestBasic.java
@@ -3,16 +3,21 @@ package org.sqlproc.engine.impl;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.cassandraunit.CassandraCQLUnit;
+import org.cassandraunit.dataset.cql.ClassPathCQLDataSet;
+import org.junit.Rule;
 import org.junit.Test;
 
 import com.datastax.driver.core.ResultSet;
 
 public class TestBasic extends TestDatabase {
 
+    @Rule
+    public CassandraCQLUnit basicCQLUnit = new CassandraCQLUnit(new ClassPathCQLDataSet("simple.cql", "basic"));
+
     @Test
     public void testSimple() {
-        ResultSet result = cassandraCQLUnit.session
-                .execute("select * from testCQLTable WHERE id=1690e8da-5bf8-49e8-9583-4dff8a570737");
-        assertThat(result.iterator().next().getString("value"), is("Cql loaded string"));
+        ResultSet result = basicCQLUnit.session.execute("select * from types WHERE id=1");
+        assertThat(result.iterator().next().getString("t_ascii"), is("ascii"));
     }
 }
--- cassandra-processor/src/test/java/org/sqlproc/engine/impl/TestDatabase.java
+++ cassandra-processor/src/test/java/org/sqlproc/engine/impl/TestDatabase.java
@@ -1,18 +1,11 @@
 package org.sqlproc.engine.impl;
 
-import org.cassandraunit.CassandraCQLUnit;
-import org.cassandraunit.dataset.cql.ClassPathCQLDataSet;
-import org.junit.Rule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TestDatabase {
 
     protected final Logger logger = LoggerFactory.getLogger(getClass());
-
-    @Rule
-    public CassandraCQLUnit cassandraCQLUnit = new CassandraCQLUnit(
-            new ClassPathCQLDataSet("simple.cql", "keyspaceSimple"));
 
     public TestDatabase() {
     }
